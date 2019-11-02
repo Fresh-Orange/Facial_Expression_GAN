35fa11e762124adc26c721bb97350c1d0377b7ea
fresh-orange <754578682@qq.com>
2019-11-02 14:20:22 +0800

    matting版本v2---调整了身体扩展大小

--- fusion/bg_move_fusion.py
+++ fusion/bg_move_fusion.py
@@ -46,64 +46,6 @@ def fusion(knockout_bg, bg_bbox, face, face_bbox):
 
     return white_bord
 
-def stepBorderV2(src, top, bottom, left, right, step):
-    step_num = top // step + 1
-    padding = src
-    padding = cv2.copyMakeBorder(padding, top, bottom, left, right,
-                                 borderType=cv2.BORDER_REPLICATE)
-    top_slice = src[:step]
-    top_block = numpy.tile(top_slice, (step_num, 1, 1))
-    padding[:top] = top_block[:top]
-
-    step_num = bottom // step + 1
-    bottom_slice = src[-step:]
-    bottom_block = numpy.tile(bottom_slice, (step_num, 1, 1))
-    padding[-bottom:] = bottom_block[-bottom:]
-
-    step_num = left // step + 1
-    left_slice = src[:, :left]
-    left_block = numpy.tile(left_slice, (1, step_num, 1))
-    padding[:, :left] = left_block[:, :left]
-
-    step_num = right // step + 1
-    right_slice = src[:, -right:]
-    right_block = numpy.tile(right_slice, (step_num, 1, 1))
-    padding[:, -right:] = right_block[:, -right:]
-
-    return padding
-
-def stepBorder(src, top, bottom, left, right, step):
-    step_num = top // step
-    padding = src
-    for i in range(step_num):
-        padding = cv2.copyMakeBorder(padding, step, 0, 0, 0,
-                                       borderType=cv2.BORDER_REFLECT101)
-    padding = cv2.copyMakeBorder(padding, top % step, 0, 0, 0,
-                                 borderType=cv2.BORDER_REFLECT101)
-
-    step_num = bottom // step
-    for i in range(step_num):
-        padding = cv2.copyMakeBorder(padding, 0, step, 0, 0,
-                                     borderType=cv2.BORDER_REFLECT101)
-    padding = cv2.copyMakeBorder(padding, 0, bottom % step, 0, 0,
-                                 borderType=cv2.BORDER_REFLECT101)
-
-    step_num = left // step
-    for i in range(step_num):
-        padding = cv2.copyMakeBorder(padding, 0, 0, step, 0,
-                                     borderType=cv2.BORDER_REFLECT101)
-    padding = cv2.copyMakeBorder(padding, 0, 0, left % step, 0,
-                                 borderType=cv2.BORDER_REFLECT101)
-
-    step_num = right // step
-    for i in range(step_num):
-        padding = cv2.copyMakeBorder(padding, 0, 0, 0, step,
-                                     borderType=cv2.BORDER_REFLECT101)
-    padding = cv2.copyMakeBorder(padding, 0, 0, 0, left % step,
-                                 borderType=cv2.BORDER_REFLECT101)
-
-    return padding
-
 
 def knn_fusion(bg, body, body_alpha, bg_bbox, face, face_bbox):
     bg_bx, bg_by, bg_bw, bg_bh = [int(v) for v in bg_bbox]
@@ -111,7 +53,7 @@ def knn_fusion(bg, body, body_alpha, bg_bbox, face, face_bbox):
     # print("bg_bbox", bg_bbox)
     # print("face_bbox", face_bbox)
     heigth, width, _ = body.shape
-    middle_shift = int(bg_bx + bg_bw/2 - width/2) # 人脸的中心偏离图像中心的大小
+    middle_shift = int(bg_bx + bg_bw/2 - width/2)
 
     white_bord = numpy.ones_like(bg)
 
@@ -128,16 +70,19 @@ def knn_fusion(bg, body, body_alpha, bg_bbox, face, face_bbox):
     no_pad_h, no_pad_w , _ = body.shape
 
     #body = cv2.copyMakeBorder(body, 1000, 1000, 1000, 1000, cv2.BORDER_REPLICATE)  # 身体向下填充 pad only bottom
-    step = 5
-    left_padding = max(bg_bw // 5 - middle_shift, 0)
-    right_padding = max(bg_bw // 5 + middle_shift, 0)
-    body = stepBorderV2(body, 0, 1000, 0, 0, step)  # 身体向下填充 pad only bottom
-    body = stepBorderV2(body, 0, 0, left_padding, right_padding, step)  # 身体向下填充 pad only bottom
-    body = cv2.copyMakeBorder(body, 1000, 0, 1000-(left_padding), 1000-(right_padding), value=0, borderType=cv2.BORDER_CONSTANT)  # 身体向下填充 pad only bottom
+    left_padding = max(0, bg_bw//5 - middle_shift)
+    right_padding = max(0, bg_bw//5 + middle_shift)
+    body = cv2.copyMakeBorder(body, 0, 1000, 0, 0,
+                              borderType=cv2.BORDER_REPLICATE)  # 身体向下填充 pad only bottom
+    body = cv2.copyMakeBorder(body, 0, 0, left_padding, right_padding,
+                              borderType=cv2.BORDER_REPLICATE)  # 身体向下填充 pad only bottom
+    body = cv2.copyMakeBorder(body, 1000, 0, 1000-left_padding, 1000-right_padding, value=0, borderType=cv2.BORDER_CONSTANT)  # 身体向下填充 pad only bottom
 
-    body_alpha = stepBorderV2(body_alpha, 0, 1000, 0, 0, step)  # 身体向下填充 pad only bottom
-    body_alpha = stepBorderV2(body_alpha, 0, 0, left_padding, right_padding, step)  # 身体向下填充 pad only bottom
-    body_alpha = cv2.copyMakeBorder(body_alpha, 1000, 0, 1000 - (left_padding), 1000 - (right_padding), value=0,
+    body_alpha = cv2.copyMakeBorder(body_alpha, 0, 1000, 0, 0,
+                              borderType=cv2.BORDER_REPLICATE)  # 身体向下填充 pad only bottom
+    body_alpha = cv2.copyMakeBorder(body_alpha, 0, 0, left_padding, right_padding,
+                              borderType=cv2.BORDER_REPLICATE)  # 身体向下填充 pad only bottom
+    body_alpha = cv2.copyMakeBorder(body_alpha, 1000, 0, 1000 - left_padding, 1000 - right_padding, value=0,
                               borderType=cv2.BORDER_CONSTANT)  # 身体向下填充 pad only bottom
 
     #cv2.imwrite("knockout_bg.jpg", knockout_bg)
